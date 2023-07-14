978488635f4dbbd61cf59198f7d6bf243bbdeb33
Khan Ramsha <khanramsha302020@gmail.com.com>
2023-07-14 12:18:13 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -25,11 +30,16 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils-Light Mode";
+      setTimeout(() => {
+        document.title = "TextUtils  is Amazing";
+      }, 1500)
     }
     else {
       setMode('dark');
       document.body.style.backgroundColor = '#041033';
       showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils-Dark Mode";
     }
   }
 
@@ -54,18 +64,27 @@ function App() {
   }
   return (
     <>
-
-      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} toggleModee={toggleModee} toggleModee2={toggleModee2} toggleModee3={toggleModee3} toggleModee4={toggleModee4} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the Text to analyze" mode={mode} />
-      </div>
-      <About />
+      <Router>
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} toggleModee={toggleModee} toggleModee2={toggleModee2} toggleModee3={toggleModee3} toggleModee4={toggleModee4} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route path='/' element={<TextForm showAlert={showAlert} heading="Enter the Text to analyze" mode={mode} />}></Route> 
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
 export default App;
 
+// sabse pehle imports dekhle
+// pehle saari cheezo ko Router tag me daalne ka
+// fir jo teri links h na usko Routes tag me daalne ka
+// ab fir jo apne links honge na navbar ke usko apan Route wale tag me daalenge
+// saare tag similar h to yaad rkhna konsa kaha lagta hai
 
+// syntax
+{/* <Route path='/' element={}></Route> */}
 
-
+//ye ho gaya apna
+//lekin abhi link banana baaki hai wo apan navbar me banayenge
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ export default function TextForm(props) {
   const handleOnChange = (event) => {
     console.log('On Change');
     setText(event.target.value);
+    document.title = "TextUtils-TextForm";
   }
   const handleOnClick = () => {
     let newtext = text.toUpperCase();
@@ -14,7 +15,7 @@ export default function TextForm(props) {
     navigator.clipboard.writeText(text)
       .then(() => {
         console.log('Text copied to clipboard');
-        props.showAlert('Text copied to clipboard','success');
+        props.showAlert('Text copied to clipboard', 'success');
       })
       .catch((error) => {
         console.error('Failed to copy text to clipboard:', error);
@@ -63,29 +64,30 @@ export default function TextForm(props) {
   const [text, setText] = useState('');
   return (
     <>
-      <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'light' }}>
-        <h1 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h1>
-        <div className="mb-3">
-          <label htmlFor="mybox" style={{ color: props.mode === 'light' ? 'black' : 'white' }} className="form-label">Example textarea</label>
-          <textarea className="form-control" onChange={handleOnChange} style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white', border: '1px solid black', boxShadow: '3px 5px' }} value={text} id="mybox" rows="8"></textarea>
+      <div className="container my-3">
+        <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'light' }}>
+          <h1 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h1>
+          <div className="mb-3">
+            <label htmlFor="mybox" style={{ color: props.mode === 'light' ? 'black' : 'white' }} className="form-label">Example textarea</label>
+            <textarea className="form-control" onChange={handleOnChange} style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white', border: '1px solid black', boxShadow: '3px 5px' }} value={text} id="mybox" rows="8"></textarea>
+          </div>
+          <button className='btn btn-primary mx-2' onClick={handleOnClick}>Convert to Uppercase</button>
+          <button className='btn btn-primary mx-2' onClick={handleOnClickl}>Convert to Lowercase</button>
+          <button className='btn btn-primary mx-2' onClick={handleclearClick}>Clear Text</button>
+          <button className='btn btn-primary mx-2' onClick={handleremoveClick}>Remove extraspace</button>
+          <button className='btn btn-primary mx-2' onClick={handlecopyClick}>Copy text to clipboard</button>
+          <button className='btn btn-primary mx-2' onClick={handlecapClick}>Capitalize Text</button>
+          <button className='btn btn-primary my-2 mx-2' onClick={handleinverseClick}>Inverse Text</button>
+
         </div>
-        <button className='btn btn-primary mx-2' onClick={handleOnClick}>Convert to Uppercase</button>
-        <button className='btn btn-primary mx-2' onClick={handleOnClickl}>Convert to Lowercase</button>
-        <button className='btn btn-primary mx-2' onClick={handleclearClick}>Clear Text</button>
-        <button className='btn btn-primary mx-2' onClick={handleremoveClick}>Remove extraspace</button>
-        <button className='btn btn-primary mx-2' onClick={handlecopyClick}>Copy text to clipboard</button>
-        <button className='btn btn-primary mx-2' onClick={handlecapClick}>Capitalize Text</button>
-        <button className='btn btn-primary my-2 mx-2' onClick={handleinverseClick}>Inverse Text</button>
-
-      </div>
-      <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
-        <h2>Your Text Summary</h2>
-        <p>{text.length > 0 ? text.trim().split(/\s+/).length : "0"} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes</p>
-        <h1>Preview</h1>
-        <p>Your entered text: {text.length > 0 ? text : 'Enter something'}</p>
-
+        <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+          <h2>Your Text Summary</h2>
+          <p>{text.length> 0 ? text.trim().split(/\s+/).length : "0"} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} Minutes</p>
+          <h1>Preview</h1>
+          <p>Your entered text: {text.length > 0 ? text : 'Enter something'}</p>
+        </div>
       </div>
     </>
   )
-}
+  }
